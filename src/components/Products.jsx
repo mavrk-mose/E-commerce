3c00b9dadf0d82c701c0d4ef7a70cd9d3cd832a1
mavrk-mose <kippemoses25@gmail.com>
2022-11-18 12:36:40 +0300

    added a grid display to product page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -23,6 +23,26 @@ function Products() {
                     <img src="https://images.unsplash.com/photo-1606406054219-619c4c2e2100?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="girl" />
                     <h3>Product description</h3>
                     <h2>Price: $45</h2>
+                </div>
+                <div className="product">
+                    <img src="https://images.unsplash.com/photo-1606406054219-619c4c2e2100?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="girl" />
+                    <h3>Product description</h3>
+                    <h2>Price: $45</h2>
+                </div>
+                <div className="product">
+                    <img src="https://images.unsplash.com/photo-1606406054219-619c4c2e2100?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="girl" />
+                    <h3>Product description</h3>
+                    <h2>Price: $45</h2>
+                </div>
+                <div className="product">
+                    <img src="https://images.unsplash.com/photo-1606406054219-619c4c2e2100?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="girl" />
+                    <h3>Product description</h3>
+                    <h2>Price: $45</h2>
+                </div>
+                <div className="product">
+                    <img src="https://images.unsplash.com/photo-1606406054219-619c4c2e2100?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="girl" />
+                    <h3>Product description</h3>
+                    <h2>Price: $45</h2>
                 </div>      
     </div>
   )
